5322924f3c2dfc6fb1bbdc2ecf561ae4d41baa3b
Pang Chong <ynpangchong@gmail.com>
2014-12-23 19:36:50 +0900

    added test case.

--- controller_test.go
+++ controller_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -26,7 +27,12 @@ func TestCreateLearningObject(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(CreateLearningObject))
 	defer ts.Close()
 
-	res, err := http.Get(ts.URL)
+	values := make(url.Values)
+	values.Set("Test1", "Test1")
+	values.Set("password1", "password1")
+	values.Set("password2", "password2")
+
+	res, err := http.PostForm(ts.URL, values)
 	if err != nil {
 		t.Error("unexpected")
 		return
